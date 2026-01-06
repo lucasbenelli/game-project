393c70f704933372c803c7c00843976b753fb41f
lucasbenelli <lucasbmmenezes@gmail.com>
2026-01-06 09:46:38 -0500

    updated every file

--- home.js
+++ home.js
@@ -1,1 +1,23 @@
-let button = document.querySelector("button");
+let button = document.querySelector("#show-games");
+let gameData = document.querySelector();
+
+button.addEventListener('click',function(){
+    const container = document.getElementById("games");
+      container.innerHTML = "";
+
+      gamesData.forEach(game => {
+        const card = document.createElement("div");
+        card.className = "game-card";
+
+        card.innerHTML = `
+          <h3>${game.title}</h3>
+          <p><span class="label">Publisher:</span> ${game.publisher}</p>
+          <p><span class="label">Genre:</span> ${game.genre}</p>
+          <p><span class="label">Score:</span> ${game.score}</p>
+        `;
+
+        container.appendChild(card);
+      });
+    
+});
+      
